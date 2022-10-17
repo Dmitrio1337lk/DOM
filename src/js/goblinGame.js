db1368cbb8a8f9f1c2eac5e595752d976e1307d1
dmitriy1337lk <dmitriy1337lk@gmail.com>
2022-10-17 12:54:12 +0200

    fix 2

--- src/js/goblinGame.js
+++ src/js/goblinGame.js
@@ -1,40 +1,66 @@
 const squares = document.querySelectorAll('.square');
 const timeLeft = document.querySelector('#timeLeft');
 const score = document.querySelector('#score');
+
 let result = 0;
 let hitPosition;
 let currentTime = 90;
-function randomSquare() {
+
+function randomSquare(){
+
     squares.forEach(square => {
         square.classList.remove('goblin');
     })
+
     let randomBox = squares[Math.floor(Math.random() * 16)];
+
     randomBox.classList.add('goblin');
+
     hitPosition = randomBox.id;
+
 }
 
 squares.forEach(square => {
+
     square.addEventListener('mousedown', () => {
+
         if (square.id == hitPosition) {
             result;
             console.log(result++);
             score.textContent = result;
             hitPosition = null;
         }
+
     })
+
 })
-function moveGoblin() {
+
+function moveGoblin(){
+
     let timerId = null;
+
     timerId = setInterval(randomSquare, 500);
+
 }
+
 moveGoblin()
-function countDown() {
+
+function countDown(){
+
  currentTime--;
+
  timeLeft.textContent = currentTime;
+
  if ( currentTime == 0 ){
+
     clearInterval(countDownTimerId);
+
     alert('Время вышло! Вы молодец!');
+
     location = '';
+
  }
+
 }
+
 let countDownTimerId = setInterval(countDown, 1000)
